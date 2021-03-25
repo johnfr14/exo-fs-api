665f366807facf07d2c1a2511e820cb854e50121
johnfr14 <jonathan.alyra@gmail.com>
2021-03-25 19:29:13 +0100

    cp

--- cp.js
+++ cp.js
@@ -1,12 +1,22 @@
 const fs = require('fs')
 const readline = require('readline-sync')
 
+
+// recupere nos parametre dans 2 variable distinct
+let srcName = process.argv[2]
+if (srcName.split('/').length > 1) {
+  srcName = srcName.split('/').pop()         // on recurer le nom du fichier à copié dans le cas où nous ne précisons pas le nouveau nom lors du collage (il le prendra par default alors)
+}
+let destinationName = process.argv[3] === '.' ? srcName : process.argv[3]  // on sait tous ce que signifie ce petit point tout mimi <3 si il n'est pas present on garde la destination tel quelle
+
 // check if command line is well 
 if (process.argv.length !== 4) {
   console.log('Veullez ne mettre que "Le fichier à copié" et "la destination où il sera copié"\n')
   console.log('Example: node cp.js leFichieràcopier.js ../onLeColleIci.js ')
   process.exit(1)
 }
+
+//-------------------- CHECK PARAMETRE 1-----------------------------------//
 
 // check if the path to copy exist
 if (!fs.existsSync(process.argv[2])) {
@@ -20,25 +30,38 @@ if (!stats.isFile()) {
   console.log(`${process.argv[2]} n\'est pas un fichier`)
   process.exit(1)
 }
+//-------------------------------------------------------------------------//
 
-// check if the destination is a directory
-const stats2 = fs.statSync(process.argv[3])
+
+
+
+//----------------------CHECK PARAMETRE 2----------------------------------//
+
+// check if the 3rd parameter is an existing directory/file if not paste it in the current directory
+if (!fs.existsSync(destinationName)) {
+  fs.copyFileSync(process.argv[2],destinationName)
+  process.exit(1)
+}
+
+// now we will check if it is directory if yes we will check if the file already exist
+const stats2 = fs.statSync(destinationName)
 if (stats2.isDirectory()) {
-  if (fs.existsSync(`${process.argv[3]}/${process.argv[3]}`)) {
-  console.log(`Désoler ${process.argv[3]} existe déjà`)
+
+  if (fs.existsSync(`${destinationName}/${srcName}`)) {
+  console.log(`Désoler ${srcName} existe déjà`)
   let answer = readline.question('Voulez vous le remplacer ?\nyes [y]\nno [n]\nVotre réponse : ')
   answer === 'y' ? '' : process.exit(1)
   } 
-  fs.copyFileSync(process.argv[2],`${process.argv[3]}/${process.argv[2]}`)
+  fs.copyFileSync(process.argv[2],`${destinationName}/${srcName}`)
   process.exit(1)
 }
 
 // check if the file we want to past already exist
 // if yes then the user will be able to choose to replace it or not
-if (fs.existsSync(process.argv[3])) {
-  console.log(`Désoler ${process.argv[3]} existe déjà`)
+if (fs.existsSync(destinationName)) {
+  console.log(`Désoler ${destinationName} existe déjà`)
   let answer = readline.question('Voulez vous le remplacer ?\nyes [y]\nno [n]\nVotre réponse : ')
   answer === 'y' ? '' : process.exit(1)
 }
 
-fs.copyFileSync(process.argv[2],process.argv[3])
+fs.copyFileSync(process.argv[2],destinationName)
