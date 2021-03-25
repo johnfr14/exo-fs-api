0c284af2a7499d3c86117c0ea67af1db0d3bca86
johnfr14 <jonathan.alyra@gmail.com>
2021-03-25 17:23:04 +0100

    coucou

--- cp.js
+++ cp.js
@@ -16,9 +16,20 @@ if (!fs.existsSync(process.argv[2])) {
 
 //check if the value is a file or a directory 
 const stats = fs.statSync(process.argv[2])
-
-if (!stats.isFile(process.argv[2])) {
+if (!stats.isFile()) {
   console.log(`${process.argv[2]} n\'est pas un fichier`)
+  process.exit(1)
+}
+
+// check if the destination is a directory
+const stats2 = fs.statSync(process.argv[3])
+if (stats2.isDirectory()) {
+  if (fs.existsSync(`${process.argv[3]}/${process.argv[3]}`)) {
+  console.log(`Désoler ${process.argv[3]} existe déjà`)
+  let answer = readline.question('Voulez vous le remplacer ?\nyes [y]\nno [n]\nVotre réponse : ')
+  answer === 'y' ? '' : process.exit(1)
+  } 
+  fs.copyFileSync(process.argv[2],`${process.argv[3]}/${process.argv[2]}`)
   process.exit(1)
 }
 
