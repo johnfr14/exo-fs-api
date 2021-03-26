c258edcc8c02c7a49a81f846d4f2dd64aac52114
johnfr14 <jonathan.alyra@gmail.com>
2021-03-26 09:15:35 +0100

    push

--- wc.js
+++ wc.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const FILE = process.argv.length - 1
+const FILE = process.argv[process.argv.length - 1]
 
 // check if command line is well 
 if (process.argv.length < 3 || process.argv.length > 4) {
@@ -9,28 +9,28 @@ if (process.argv.length < 3 || process.argv.length > 4) {
 }
 
 // check if the path exist
-if (!fs.existsSync(process.argv[FILE])) {
-  console.log(`Désoler ${process.argv[FILE]} n\'existe pas`)
+if (!fs.existsSync(FILE)) {
+  console.log(`Désoler ${FILE} n\'existe pas`)
   process.exit(1)
 }
 
 // Recuperation du contenu du fichier
-const TEXT = fs.readFileSync(process.argv[FILE], 'utf-8')
+const TEXT = fs.readFileSync(FILE, 'utf-8')
 
 // Si Flag le est activé
 if (process.argv.length === 4) {
   switch (process.argv[2]) {
     case '-l':
       let countLine = TEXT.split("\n").length
-      console.log(`${countLine} ${process.argv[FILE]}`)
+      console.log(`${countLine} ${FILE}`)
       break
     case '-w':
       let countWord = TEXT.split(" ").length
-      console.log(`${countWord} ${process.argv[FILE]}`)
+      console.log(`${countWord} ${FILE}`)
       break
     case '-c':
       let countChar = TEXT.split("").length
-      console.log(`${countChar} ${process.argv[FILE]}`)
+      console.log(`${countChar} ${FILE}`)
       break
     default:
       console.log('Pour l\'utilisation d\'un flag veuillez mettre en premier parametre ["-l", "-w", "-c"] puis le nom du fichier')
@@ -43,4 +43,4 @@ let countLine = TEXT.split("\n").length
 let countWord = TEXT.split(" ").length
 let countChar = TEXT.split("").length
 
-console.log(`${countLine} ${countWord} ${countChar} ${process.argv[FILE]}`)
+console.log(`${countLine} ${countWord} ${countChar} ${FILE}`)
